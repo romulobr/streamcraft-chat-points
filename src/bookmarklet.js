9f86bb4b25266f9fd6447d41538d044a5192a492
Romulo <romulo.santos@king.com>
2019-04-16 20:50:15 +0200

    fixes zica for real.

--- src/bookmarklet.js
+++ src/bookmarklet.js
@@ -1,4 +1,4 @@
-const sent = [];
+let sent = [];
 
 function newMessagesFilter(element) {
     return sent.indexOf(element) < 0;
@@ -10,13 +10,16 @@ function update() {
     xhr.setRequestHeader('Content-Type', 'application/json');
     xhr.onreadystatechange = function () {
         if (this.readyState === 4) {
-            console.log('sent');
+            console.log('done');
         }
     };
     const chatMessages = Array.prototype.slice.call(document.querySelectorAll('.chat-list li')).map(li => li.innerHTML);
     const newMessages = chatMessages.filter(newMessagesFilter);
-    sent.concat(chatMessages);
-    xhr.send(JSON.stringify({value: newMessages}));
+    sent = sent.concat(chatMessages);
+    if (newMessages.length > 0) {
+        xhr.send(JSON.stringify({value: newMessages}));
+        console.log('sending ', newMessages.length);
+    }
 }
 
 setInterval(update, 30000);
